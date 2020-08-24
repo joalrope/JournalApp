9be973b32f43dc3b4da280fab0e12454475b6af2
Jose Rodriguez <carlosjavierbermudez@MACCB.local>
2020-08-24 11:22:09 -0400

    Estructura, rutas y estilos agregado

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { AppRouter } from './components/routers/AppRouter';
 
 export const JournalApp = () => {
     return (
         <div>
-            <h1>JournalApp</h1>
-            <hr/>
+            <AppRouter />
         </div>
     )
 }
---
+++ src/components/routers/AppRouter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { AuthRouter } from './AuthRouter';
+import { JournalScreen } from '../journal/JournalScreen';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+export const AppRouter = () => {
+    return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route path="/auth" component={AuthRouter} />
+                        <Route exact path="/" component={JournalScreen} />
+
+                        <Redirect to='/auth/login' />
+                    </Switch>
+                </div>
+            </Router>
+    )
+}

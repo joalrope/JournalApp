1bd1c77d0ce8ec4cd3a9a69db25ea78acc5281fd
Jose Rodriguez <carlosjavierbermudez@MACCB.local>
2020-09-04 12:09:39 -0400

    Asignacion de Imagen colocada

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -32,37 +32,35 @@ export const NoteScreen = () => {
         <div className="notes__main-content">
             <NotesAppBar />
 
-            <div className="notes__edit-content">
-                {
-                    note.url
-                    && (
-                        <div className="notes__image">
-                            <img src= {note.url} alt="painting"/>
-                        </div>
-                        )
-                }
-    
-                <div className="notes__content">
-                    
-                    <input
-                        type="text"
-                        placeholder="algún título asombroso"
-                        className="notes__title-input"
-                        autoComplete="off"
-                        name= "title"
-                        value= {title}
-                        onChange= {handleInputChange}
-                    />
-    
-                    <textarea
-                        placeholder="Que paso Hoy?"
-                        className="notes__textarea"
-                        name= "body"
-                        value= {body}
-                        onChange= {handleInputChange}
-                    ></textarea>
-                </div>
+            <div className="notes__content">
+                
+                <input
+                    type="text"
+                    placeholder="algún título asombroso"
+                    className="notes__title-input"
+                    autoComplete="off"
+                    name= "title"
+                    value= {title}
+                    onChange= {handleInputChange}
+                />
+
+                <textarea
+                    placeholder="Que paso Hoy?"
+                    className="notes__textarea"
+                    name= "body"
+                    value= {body}
+                    onChange= {handleInputChange}
+                ></textarea>
             </div>
+            
+            {
+                note.url
+                && (
+                    <div className="notes__image">
+                        <img src= {note.url} alt="painting"/>
+                    </div>
+                    )
+            }            
         </div>
     );
 };
--- src/components/notes/NotesAppBar.jsx
+++ src/components/notes/NotesAppBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import moment from 'moment';
-import { startSaveNote } from '../../actions/notes';
+import { startSaveNote, startUploading } from '../../actions/notes';
 
 
 export const NotesAppBar = () => {
@@ -18,14 +18,38 @@ export const NotesAppBar = () => {
         dispatch(startSaveNote(active));
     }
 
+    const handlePictureClick = () => {
+        document.querySelector('#fileSelector').click();
+    }
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            dispatch(startUploading(file));
+        }
+    }
+
 
     return (
         <div className="notes__appbar">
 
+            <input
+                id= "fileSelector"
+                type="file"
+                name= ""
+                style= {{display: 'none'}}
+                onChange= {handleFileChange} />
+
             <span>{noteDate}</span>
             
             <div>
-                <button className="btn btn-outline">Imagen</button>
+                <button
+                    className="btn btn-outline"
+                    onClick= {handlePictureClick}
+                >
+                    Imagen
+                </button>
+                
                 <button
                     className="btn btn-outline"
                     onClick= { handleSaveNote }
